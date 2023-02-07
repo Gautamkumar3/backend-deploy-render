e4922c0e5c0d10e71d4eedd007c07093d8386ec7
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-02-07 19:05:07 +0530

    update backend

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Connect = () => {
-    return mongoose.connect("mongodb+srv://gautam:gautam@cluster0.clfb7k7.mongodb.net/blogs")
-}
+  return mongoose.connect(
+    "mongodb+srv://gautam:gautam@cluster0.clfb7k7.mongodb.net/blogs"
+  );
+};
 
 module.exports = Connect;
