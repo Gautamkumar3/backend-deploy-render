e5a95390b7435d99a82f225d8b4aad8a69580727
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-02-20 09:59:59 +0530

    changes done

--- src/comment/comment.schema.js
+++ src/comment/comment.schema.js
@@ -1,21 +1,30 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const CommentSchema = new mongoose.Schema({
+const CommentSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
-    comments: { type: String, required: true },
-    postId: { type: mongoose.Schema.Types.ObjectId, ref: "post", required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-},
+    comments: { type: String, required: [true, "Comment is missing"] },
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
 
-    {
-        versionKey: false,
-        timestamps: {
-            createdAt: 'created_at',
-            updatedAt: 'updated_at'
-        }
-    }
-)
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
-const Comment = mongoose.model("comment", CommentSchema)
+const Comment = mongoose.model("comment", CommentSchema);
 
 module.exports = Comment;
